8822245b717509502830eea7b06b9a090580a933
Andrey Grechkin <grechkin@difway.studio>
2025-11-17 17:27:15 +0300

    first commit

---
+++ composeApp/src/commonTest/kotlin/com/defey/labpuzzles/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.defey.labpuzzles
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/defey/labpuzzles/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.defey.labpuzzles
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
